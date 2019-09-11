b8016e4bf37ca28afd6cd7aa4213081d5362dfeb
S P Nithesh Kumar <spnitesh1999@gmail.com>
2019-09-11 13:10:52 +0530

    first commit

---
+++ fp.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+int main(void)
+{
+    printf(1,"WOW It really works\n");
+    printf(1,"It is always fun to work with xv6\n");
+    exit();
+}
+
---
+++ myprog.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+int main(void)
+{
+    printf(1,"My first user program in xv6");
+    exit();
+}
